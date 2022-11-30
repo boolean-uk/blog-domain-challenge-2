60c39257b183fe26f018a835aaf2d023edd138fb
Alexander Lind <alex.anonym@gmail.com>
2022-11-30 15:03:17 +0100

    req 3 done

---
+++ prisma/migrations/20221130123855_added_user_model_and_seed/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[userName]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "userName" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userName_key" ON "User"("userName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,53 +1,83 @@
-// const { PrismaClient } = require("@prisma/client");
-// const prisma = new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-// async function main() {
-//   const alice = await prisma.user.upsert({
-//     where: { email: "alice@prisma.io" },
-//     update: {},
-//     create: {
-//       email: "alice@prisma.io",
-//       name: "Alice",
-//       posts: {
-//         create: {
-//           title: "Check out Prisma with Next.js",
-//           content: "https://www.prisma.io/nextjs",
-//           published: true,
-//         },
-//       },
-//     },
-//   });
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      userName: "AliceX1992",
+      password: "Password1234",
+      profile: {
+        create: {
+          firstName: "Alice",
+          lastName: "Hanson",
+          age: 18,
+          pictureUrl: "https://picsum.photos/200",
+        },
+      },
+    },
+    include: {
+      profile: true,
+      posts: true,
+      comments: true,
+    },
+  });
 
-//   const bob = await prisma.user.upsert({
-//     where: { email: "bob@prisma.io" },
-//     update: {},
-//     create: {
-//       email: "bob@prisma.io",
-//       name: "Bob",
-//       posts: {
-//         create: [
-//           {
-//             title: "Follow Prisma on Twitter",
-//             content: "https://twitter.com/prisma",
-//             published: true,
-//           },
-//           {
-//             title: "Follow Nexus on Twitter",
-//             content: "https://twitter.com/nexusgql",
-//             published: true,
-//           },
-//         ],
-//       },
-//     },
-//   });
-//   console.log({ alice, bob });
-// }
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+
+
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      userName: "BobIsKing",
+      password: "eatmyshorts!.1823",
+      profile: {
+        create: {
+          firstName: "Bob",
+          lastName: "Ellison",
+          age: 22,
+          pictureUrl: "https://picsum.photos/200",
+        },
+      },
+      posts: {
+        create: {
+          title: "Why JavaScript sucks",
+          content: "Because I dont understand it...",
+          imageUrl: "https://picsum.photos/200",
+          publishedAt: new Date(Date.now()),
+        },
+      },
+    },
+  });
+
+  const alicePost = await prisma.post.create({
+    data: {
+      title: "Why Python sucks",
+      content: "Because I dont understand it...",
+      imageUrl: "https://picsum.photos/200",
+      publishedAt: new Date(Date.now()),
+      comments: {
+        create: {
+          content: "Python > JavaScript all day!",
+          user: { connect: { id: bob.id } },
+        },
+      },
+      categories: {
+        create: [{name: "Python"}, {name: "JavaScript"}, {name: "Coding"}]
+      },
+      user: { connect: { id: alice.id } },
+    },
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
